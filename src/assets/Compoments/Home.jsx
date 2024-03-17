473f94df52d6601941951021d523da4400544bf6
ArtusAbelard <tribelha@gmail.com>
2024-03-17 18:30:01 +0100

    trying good index

--- src/assets/Compoments/Home.jsx
+++ src/assets/Compoments/Home.jsx
@@ -15,13 +15,7 @@ export default function Home(props) {
         dispatch(panierset(Element.name))
         dispatch(prixset(Element.price)) 
     }
-    // const Filter = (params) => {
-    //     setpizza(pizza.sort((a,b) => {
-    //         return a.price - b.price
-    //     }))
-    // }
-    const sortedPizzas = [...pizza].sort((a, b) => a.price - b.price);
-    console.log(sortedPizzas);
+   
     function sortedpizza(params) {
         setpizza([...pizza].sort((a, b) =>  b.price - a.price))
         
@@ -30,6 +24,11 @@ export default function Home(props) {
         setpizza([...pizza].sort((a, b) =>  a.price - b.price))
         
     }
+    const chercherIndex = (Element) => {
+        const i = pizza.findIndex((x) => x.name === Element.name);
+        return i;
+        
+    };
 
     return (
         <div className=' w-screen h-screen relative flex flex-col items-center  '>
@@ -44,7 +43,7 @@ export default function Home(props) {
                                     <div onClick={()=>{setlesenroulles(Element)}} className='cursor-pointer h-[40px] w-[90px] rounded-2xl top-6 text-xl left-4 bg-black absolute hover:bg-[#DA291C] text-white flex items-center justify-center'><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>{Element.price} €</div>
                                     <img className='h-[150%] pt-2' src={Element.img} alt="Shoes" />
                                 </figure>
-                                <Link to={`/Details/${id}`}>
+                                <Link to={`/Details/${chercherIndex(Element)}`}>
                                 <div className="card-body h-[6rem] hover:bg-black rounded-b-xl hover:text-white flex justify-center items-center">
                                     <h2 className="card-title text-center text-2xl">{Element.name}</h2>
                                     
